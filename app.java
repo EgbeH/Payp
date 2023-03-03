52c3b4b7b6d4c6169a6b710e1be8b307d3f76763
egbe <egbe90@yahoo.com>
2023-03-03 10:12:58 -0600

    concluded

--- app.java
+++ app.java
@@ -3,8 +3,10 @@ developing java apps
 Great 
 great
 today is friday
- HEAD
 the app is excellent for users
 the app is just right
 the app is testing 2
 1e1151f (merging conf 2)
+the app is excellent for users
+the app is just right
+fc1e59b (merging conf)
