97ec2f8d5d821e7c7e79eb8a5b09f5cfa1ba6612
EgbeH <126033724+EgbeH@users.noreply.github.com>
2023-03-04 16:17:10 -0600

    Update app.java
    2nd update

--- app.java
+++ app.java
@@ -5,8 +5,4 @@ great
 today is friday
 the app is excellent for users
 the app is just right
-the app is testing 2
-merging conf 2
-the app is excellent for users
-the app is just right
-merging conf
+the app is testing
